665ab72603237ce51abfa02f50c0f13c7525b135
Aaron Parecki <aaron@parecki.com>
2020-12-15 10:50:16 -0800

    formatting

--- youtube.js
+++ youtube.js
@@ -1,65 +1,65 @@
 $("body").unbind("click").on("click", "yt-live-chat-text-message-renderer,yt-live-chat-paid-message-renderer,yt-live-chat-membership-item-renderer", function () {
 
-	// Don't show deleted messages
-	if($(this)[0].hasAttribute("is-deleted")) {
-		console.log("Not showing deleted message");
-		return;
-	}
+  // Don't show deleted messages
+  if($(this)[0].hasAttribute("is-deleted")) {
+    console.log("Not showing deleted message");
+    return;
+  }
 
-	$(".hl-c-cont").remove();
-	var chatname = $(this).find("#author-name").text();
-	
-	// Show just the first name. Comment this out to show the full name.
-	chatname = chatname.replace(/ .*/,'');
-	
-	var chatmessage = $(this).find("#message").html();
-	var chatimg = $(this).find("#img").attr('src');
-	chatimg = chatimg.replace("32", "128");
-	var chatdonation = $(this).find("#purchase-amount").html();
-	var chatmembership = $(this).find(".yt-live-chat-membership-item-renderer #header-subtext").html();
-	$(this).addClass("show-comment");
+  $(".hl-c-cont").remove();
+  var chatname = $(this).find("#author-name").text();
 
-	var hasDonation;
-	if(chatdonation) {
-		hasDonation = '<div class="donation">' + chatdonation + '</div>';
-	} else {
-		hasDonation = '';
-	}
+  // Show just the first name. Comment this out to show the full name.
+  chatname = chatname.replace(/ .*/,'');
 
-	var hasMembership;
-	if(chatmembership) {
-		hasMembership = '<div class="donation membership">NEW MEMBER!</div>';
-		chatmessage = chatmembership;
-	} else {
-		hasMembership = '';
-	}
+  var chatmessage = $(this).find("#message").html();
+  var chatimg = $(this).find("#img").attr('src');
+  chatimg = chatimg.replace("32", "128");
+  var chatdonation = $(this).find("#purchase-amount").html();
+  var chatmembership = $(this).find(".yt-live-chat-membership-item-renderer #header-subtext").html();
+  $(this).addClass("show-comment");
 
-	var backgroundColor = "";
-	var textColor = "";
-	if(this.style.getPropertyValue('--yt-live-chat-paid-message-primary-color')) {
-		backgroundColor = "background-color: "+this.style.getPropertyValue('--yt-live-chat-paid-message-primary-color')+";";
-		textColor = "color: #111;";
-	}
+  var hasDonation;
+  if(chatdonation) {
+    hasDonation = '<div class="donation">' + chatdonation + '</div>';
+  } else {
+    hasDonation = '';
+  }
 
-	// This doesn't work yet
-	if(this.style.getPropertyValue('--yt-live-chat-sponsor-color')) {
-		backgroundColor = "background-color: "+this.style.getPropertyValue('--yt-live-chat-sponsor-color')+";";
-		textColor = "color: #111;";
-	}
+  var hasMembership;
+  if(chatmembership) {
+    hasMembership = '<div class="donation membership">NEW MEMBER!</div>';
+    chatmessage = chatmembership;
+  } else {
+    hasMembership = '';
+  }
+
+  var backgroundColor = "";
+  var textColor = "";
+  if(this.style.getPropertyValue('--yt-live-chat-paid-message-primary-color')) {
+    backgroundColor = "background-color: "+this.style.getPropertyValue('--yt-live-chat-paid-message-primary-color')+";";
+    textColor = "color: #111;";
+  }
+
+  // This doesn't work yet
+  if(this.style.getPropertyValue('--yt-live-chat-sponsor-color')) {
+    backgroundColor = "background-color: "+this.style.getPropertyValue('--yt-live-chat-sponsor-color')+";";
+    textColor = "color: #111;";
+  }
 
 
-	$( "highlight-chat" ).append('<div class="hl-c-cont fadeout"><div class="hl-name">' + chatname + '</div>' + '<div class="hl-message" style="'+backgroundColor+' '+textColor+'">' + chatmessage + '</div><div class="hl-img"><img src="' + chatimg + '"></div>'+hasDonation+hasMembership+'</div>')
-	.delay(10).queue(function(next){	
-		$( ".hl-c-cont" ).removeClass("fadeout");
-		next();
-	});	
+  $( "highlight-chat" ).append('<div class="hl-c-cont fadeout"><div class="hl-name">' + chatname + '</div><div class="hl-message" style="'+backgroundColor+' '+textColor+'">' + chatmessage + '</div><div class="hl-img"><img src="' + chatimg + '"></div>'+hasDonation+hasMembership+'</div>')
+  .delay(10).queue(function(next){
+    $( ".hl-c-cont" ).removeClass("fadeout");
+    next();
+  });
 
 });
 
 $("body").on("click", ".btn-clear", function () {
-	$(".hl-c-cont").addClass("fadeout").delay(300).queue(function(){
-		$(".hl-c-cont").remove().dequeue();
-	});
+  $(".hl-c-cont").addClass("fadeout").delay(300).queue(function(){
+    $(".hl-c-cont").remove().dequeue();
+  });
 });
 
 $( "yt-live-chat-app" ).before( '<highlight-chat></highlight-chat><button class="btn-clear">CLEAR</button>' );
