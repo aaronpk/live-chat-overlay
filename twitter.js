4ea594195a545e5c500685a7ef3ec7627b051e21
STeve Seguin <steve@seguin.email>
2021-07-29 17:59:07 -0400

    added twitter support; posts only

---
+++ twitter.js
@@ -0,0 +1,318 @@
+
+(function() {
+var soca=false;
+function generateStreamID(){
+	var text = "";
+	var possible = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789";
+	for (var i = 0; i < 10; i++){
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+};
+var channel = generateStreamID();
+var outputCounter = 0; // used to avoid doubling up on old messages if lag or whatever
+
+var sendProperties = ["color","scale","sizeOffset","commentBottom","commentHeight","authorBackgroundColor","authorAvatarBorderColor","authorColor","commentBackgroundColor","commentColor","fontFamily","showOnlyFirstName","highlightWords"];
+
+function actionwtf(){ // steves personal socket server service
+	if (soca){return;}
+
+	soca = new WebSocket("wss://api.action.wtf:666");
+	soca.onclose = function (){
+		setTimeout(function(){soca=false;actionwtf(); },2000);
+	};
+	soca.onopen = function (){
+		soca.send(JSON.stringify({"join":channel}));
+	};
+
+	chrome.storage.sync.set({
+		streamID: channel
+	});
+}
+
+setTimeout(function(){actionwtf();},100);
+
+function pushMessage(data){
+	var message = {};
+	message.msg = true;
+	message.contents = data;
+	try {
+		chrome.storage.sync.get(sendProperties, function(item){
+			outputCounter+=1;
+			message.id = outputCounter;
+			message.settings = item;
+			soca.send(JSON.stringify(message));
+		});
+	} catch(e){
+		outputCounter+=1;
+		message.id = outputCounter;
+		soca.send(JSON.stringify(message));
+	}
+}
+
+function initMessage(ele){
+	var button  = document.createElement("button");
+	button.onclick = prepMessage;
+	button.innerHTML = "Show as overlay";
+	button.style = "    width: 60px;    height: 60px;    padding: 4px;  margin: 10px; background-color: #c7f6c7; cursor:pointer;"
+	try{
+		ele.appendChild(button);
+	} catch(e){}
+}
+
+var showOnlyFirstName;
+var highlightWords = [];
+
+function toDataURL(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      callback(reader.result);
+    }
+    reader.readAsDataURL(xhr.response);
+  };
+  xhr.open('GET', url);
+  xhr.responseType = 'blob';
+  xhr.send();
+}
+
+function prepMessage(ele){
+  if (ele == window){return;}
+  if (this){
+	  ele = this.parentNode;
+  }
+  try {
+	document.querySelector(".hl-c-cont").parentNode.removeChild(document.querySelector(".hl-c-cont"));
+  } catch(e){}
+  
+  
+  var base = ele.querySelector("[data-testid='tweet']");
+  console.log(base.parentNode);
+  
+  try{
+	  var chatname = base.childNodes[1].querySelectorAll("a")[0].childNodes[0].childNodes[0].innerText;
+	  if (showOnlyFirstName) {
+		chatname = chatname.replace(/ .*/,'');
+	  }
+  } catch(e){
+	 var chatname="";
+	//return;
+  }
+  
+  try {
+	var chatmessage =  base.parentNode.childNodes[2].childNodes[0].innerText;
+  } catch(e){
+	  try{
+		  var chatmessage =  base.parentNode.childNodes[1].childNodes[1].childNodes[1].childNodes[1].innerText;
+		  if (!chatmessage.length){
+			  var chatmessage =  base.parentNode.childNodes[1].childNodes[1].childNodes[1].innerText;
+		  }
+	  } catch(e){
+		  try{
+			  var chatmessage =  base.parentNode.childNodes[1].childNodes[1].childNodes[1].innerText;
+		  } catch(e){
+			  var chatmessage="";
+		  }
+	  }
+	 console.error(e);
+	
+	//return;
+  }
+  
+  var chatimg=false;
+  try{
+	 chatimg = base.childNodes[0].querySelector("img").src
+  } catch(e){}
+  
+  var chatdonation = false;
+  var chatmembership = false;
+  var chatsticker = false;
+  
+  
+  base.style.backgroundColor = "#CCC!important";
+
+  var chatbadges = "";
+ 
+  // Mark this comment as shown
+  ele.classList.add("shown-comment");
+
+  var hasDonation = '';
+ 
+  var hasMembership = '';
+ 
+  var backgroundColor = "";
+  var textColor = "";
+ 
+  /* var chattext = $(element).text(); // add this back in
+  var chatWords = chattext.split(" ");
+  if (!highlightWords){
+	  highlightWords=[];
+  }
+  var highlights = chatWords.filter(value => highlightWords.includes(value.toLowerCase().replace(/[^a-z0-9]/gi, '')));
+  $(element).removeClass("shown-comment");
+  if(highlights.length > 0) {
+	$(element).addClass("highlighted-comment");
+  } */
+
+  var data = {};
+  data.chatname = chatname;
+  data.chatbadges = chatbadges;
+  data.backgroundColor = backgroundColor;
+  data.textColor = textColor;
+  data.chatmessage = chatmessage;
+  data.chatimg = chatimg;
+  data.hasDonation = hasDonation;
+  data.hasMembership = hasMembership;
+  data.type = "twitter";
+  
+  toDataURL(chatimg, function(dataUrl) {
+	  data.chatimg = dataUrl;
+	  pushMessage(data);
+  });
+
+  $( "highlight-chat" ).removeClass("preview").append('<div class="hl-c-cont fadeout">'
+     + '<div class="hl-name">' + chatname
+       + '<div class="hl-badges">' + chatbadges + '</div>'
+     + '</div>'
+     + '<div class="hl-message" style="'+backgroundColor+' '+textColor+'">' + chatmessage + '</div>'
+     + '<div class="hl-img"><img id="img_'+chatname+'" src="' + chatimg + '"></div>'
+     +hasDonation+hasMembership
+   +'</div>')
+  .delay(10).queue(function(next){
+    $( ".hl-c-cont" ).removeClass("fadeout");
+    next();
+  });
+};
+var loaded=false;
+
+
+$("body").on("click", ".btn-clear-twitter", function () {
+  pushMessage(false);
+  $(".hl-c-cont").addClass("fadeout").delay(300).queue(function(){
+    $(".hl-c-cont").remove().dequeue();
+  });
+});
+
+$("body").on("click", ".btn-getoverlay-twitter", function () {
+    prompt("Overlay Link: https://chat.overlay.ninja?session="+channel+"\nAdd as a browser source; set height to 250px", "https://chat.overlay.ninja?session="+channel);
+});
+
+
+// Show a placeholder message so you can position the window before the chat is live
+
+var properties = ["color","scale","streamID","sizeOffset","commentBottom","commentHeight","authorBackgroundColor","authorAvatarBorderColor","authorColor","commentBackgroundColor","commentColor","fontFamily","showOnlyFirstName","highlightWords"];
+
+chrome.storage.sync.get(properties, function(item){
+  var color = "#000";
+  if(item.color) {
+    color = item.color;
+  }
+  if (item.streamID){
+    channel = item.streamID;
+  } else {
+	chrome.storage.sync.set({
+		streamID: channel
+	});
+  }
+
+  let root = document.documentElement;
+  root.style.setProperty("--keyer-bg-color", color);
+
+  if(item.authorBackgroundColor) {
+    root.style.setProperty("--author-bg-color", item.authorBackgroundColor);
+    root.style.setProperty("--author-avatar-border-color", item.authorBackgroundColor);
+  }
+  if(item.authorAvatarBorderColor) {
+    root.style.setProperty("--author-avatar-border-color", item.authorAvatarBorderColor);
+  }
+  if(item.commentBackgroundColor) {
+    root.style.setProperty("--comment-bg-color", item.commentBackgroundColor);
+  }
+  if(item.authorColor) {
+    root.style.setProperty("--author-color", item.authorColor);
+  }
+  if(item.commentColor) {
+    root.style.setProperty("--comment-color", item.commentColor);
+  }
+  if(item.fontFamily) {
+    root.style.setProperty("--font-family", item.fontFamily);
+  }
+  if(item.scale) {
+    root.style.setProperty("--comment-scale", item.scale);
+  }
+  if(item.commentBottom) {
+    root.style.setProperty("--comment-area-bottom", item.commentBottom);
+  }
+  if(item.commentHeight) {
+    root.style.setProperty("--comment-area-height", item.commentHeight);
+  }
+  if(item.sizeOffset) {
+    root.style.setProperty("--comment-area-size-offset", item.sizeOffset);
+  }
+  showOnlyFirstName = item.showOnlyFirstName;
+  highlightWords = item.highlightWords;
+});
+
+
+function startup(containerSelector, className, callback, role=false) {
+	
+	
+		var bases = document.querySelector('main[role="main"]').querySelectorAll('article[role="article"]');
+		for (var i=0;i<bases.length;i++) {
+		  initMessage(bases[i]);
+		}
+
+	try {
+		var chatmessage = "Sample chat message!";
+		$( "highlight-chat" ).addClass("preview").append('<div class="hl-c-cont fadeout"><div class="hl-name">Sample User<div class="hl-badges"></div></div><div class="hl-message">' + chatmessage + '</div><div class="hl-img">:)</div></div>').delay(10).queue(function(next){
+			$( ".hl-c-cont" ).removeClass("fadeout");
+			next();
+		});
+	} catch(e){};
+	
+	if (loaded==false){
+		try{
+			document.querySelector('header[role="banner"]').querySelectorAll('a[aria-label="Tweet"]')[0].parentNode.outerHTML += '<button class="btn-clear-twitter">CLEAR</button><button class="btn-getoverlay-twitter" >LINK</button><highlight-chat class="highlight-twitter"></highlight-chat>';
+			loaded=true;
+		} catch(e){}
+	}
+	
+	try{
+		
+		var onMutationsObserved = function(mutations) {
+			mutations.forEach(function(mutation) {
+				if (mutation.addedNodes.length) {
+					for (var i = 0, len = mutation.addedNodes.length; i < len; i++) {
+						try{
+							if (role){
+								if(mutation.addedNodes[i].childNodes[0].childNodes[0].childNodes[0].role && (mutation.addedNodes[i].childNodes[0].childNodes[0].childNodes[0].role == role)) {
+									callback(mutation.addedNodes[i].childNodes[0].childNodes[0].childNodes[0]);
+								}
+							}
+						} catch(e){}
+					}
+				}
+			});
+			
+			if (loaded==false){
+				try{
+					document.querySelector('header[role="banner"]').querySelectorAll('a[aria-label="Tweet"]')[0].parentNode.outerHTML += '<button class="btn-clear-twitter">CLEAR</button><button class="btn-getoverlay-twitter" >LINK</button><highlight-chat class="highlight-twitter"></highlight-chat>';
+					loaded=true;
+				} catch(e){}
+			}
+		};
+		try{
+			var target = document.querySelectorAll(containerSelector)[0];
+			var config = { childList: true, subtree: true };
+			var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+			var observer = new MutationObserver(onMutationsObserved);
+			observer.observe(target, config);
+		} catch(e){console.warn(e);};
+	} catch(e){console.error(e);}
+
+}
+
+setTimeout(function(){startup("main[role='main']", false, initMessage, 'article');},2000);
+
+})();
