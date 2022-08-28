ae44dee8b1c6cfc0a1e9fa6467c21aac58419559
Aaron Parecki <aaron@parecki.com>
2022-08-28 12:12:28 -0700

    grab useful bits from 0.3 branch

--- youtube.js
+++ youtube.js
@@ -17,8 +17,10 @@ $("body").unbind("click").on("click", "yt-live-chat-text-message-renderer,yt-liv
 
   var data = {};
 
-  $(".hl-c-cont").remove();
-  data.authorname = $(this).find("#author-name").text();
+  data.message = $(this).find("#message").html();
+
+  $(this).find("#author-name #tooltip.hidden").remove();
+  data.authorName = $(this).find("#author-name").text();
 
   if(showOnlyFirstName) {
     data.authorname = data.authorname.replace(/ [^ ]+$/, '');
@@ -227,6 +229,14 @@ $("#pop-out-url").click(function(){
 });
 
 
+$(function(){
+
+  // Restore the popout URL field if they refresh the page
+  if(window.location.hash) {
+    $("#pop-out-button").click();
+  }
+
+});
 
 
 
